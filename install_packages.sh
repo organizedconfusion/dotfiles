fa49256666db3b05688809ca30d75fd597faf845
organizedconfusion <github@underclocked.com>
2026-01-02 00:35:57 -0800

    Remove install_packages.sh
    The only package installed there is handled in .vimrc now

--- install_packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo Installing yegappan/lsp
-mkdir -p $HOME/.vim/pack/downloads/opt
-cd $HOME/.vim/pack/downloads/opt
-git clone https://github.com/yegappan/lsp
-vim -u NONE -c "helptags $HOME/.vim/pack/downloads/opt/lsp/doc" -c q
-
