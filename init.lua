ce0be506c991ea88335e1910b81120ed8efcee7a
organizedconfusion <github@underclocked.com>
2026-01-23 10:12:19 -0800

    Add lazy.nvim and gruvbox for neovim

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 
+require("config.lazy")
 
 -- [[ VIM UI ]]
 vim.opt.number = true -- Print the line number in front of each line
