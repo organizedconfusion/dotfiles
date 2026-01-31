23e074c75bf5f17ab12f3fa5676918f84a0b63ca
organizedconfusion <github@underclocked.com>
2026-01-30 20:53:58 -0800

    Add repeat moves for treesitter nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -312,6 +312,14 @@ vim.keymap.set("n", "<leader>A", function()
     require("nvim-treesitter-textobjects.swap").swap_previous "@parameter.inner"
 end)
 
+-- Tresitter repeat movements thee vim way: ; goes to the direction you were moving.
+local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 
 -- [[ AUTOCOMMANDS ]].
 -- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
