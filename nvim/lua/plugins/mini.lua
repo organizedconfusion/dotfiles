bd41435f30ea242cb19b9b2eb2c4d8fca830b321
organizedconfusion <github@underclocked.com>
2026-02-24 15:40:08 -0800

    fix some nvim stuff with snippets

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -14,45 +14,27 @@ require('mini.pick').setup({
 })
 require('mini.surround').setup()
 
-local gen_loader = require('mini.snippets').gen_loader
-require('mini.snippets').setup({
-    snippets = {
-        -- Load custom file with global snippets first (adjust for Windows)
-        gen_loader.from_file('~/.config/nvim/snippets/global.json'),
-
-        -- Load snippets based on current language by reading files from
-        -- "snippets/" subdirectories from 'runtimepath' directories.
-        gen_loader.from_lang({'c'}),
-    },
-    mappings = {
-        stop = '<ESC>',
-    }
-})
-
--- Stop session when leaving insert mode
-local make_stop = function()
-    local au_opts = { pattern = '*:n', once = true }
-    au_opts.callback = function()
-        while MiniSnippets.session.get() do
-            MiniSnippets.session.stop()
-        end
-    end
-    vim.api.nvim_create_autocmd('ModeChanged', au_opts)
-end
-local opts = { pattern = 'MiniSnippetsSessionStart', callback = make_stop }
-vim.api.nvim_create_autocmd('User', opts)
-
 -- Tab mapping
 local snippets = require('mini.snippets')
 local match_strict = function(snips)
     -- Do not match with whitespace to cursor's left
     return snippets.default_match(snips, { pattern_fuzzy = '%S+' })
 end
+
+local gen_loader = require('mini.snippets').gen_loader
 snippets.setup({
-    -- ... Set up snippets ...
     mappings = { expand = '', jump_next = '', jump_prev = '' },
     expand   = { match = match_strict },
+    snippets = {
+        -- Load custom file with global snippets first (adjust for Windows)
+        gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+
+        -- Load snippets based on current language by reading files from
+        -- "snippets/" subdirectories from 'runtimepath' directories.
+        gen_loader.from_lang(),
+    },
 })
+
 local expand_or_jump = function()
     local can_expand = #MiniSnippets.expand({ insert = false }) > 0
     if can_expand then
@@ -64,9 +46,26 @@ local expand_or_jump = function()
     end
     return '\t'
 end
+
 local jump_prev = function() MiniSnippets.session.jump('prev') end
+
 vim.keymap.set('i', '<Tab>', expand_or_jump, { expr = true })
 vim.keymap.set('i', '<S-Tab>', jump_prev)
+
+-- Stop session when leaving insert mode
+local make_stop = function()
+    local au_opts = { pattern = '*:n', once = true }
+    au_opts.callback = function()
+        while MiniSnippets.session.get() do
+            MiniSnippets.session.stop()
+        end
+    end
+    vim.api.nvim_create_autocmd('ModeChanged', au_opts)
+end
+
+local opts = { pattern = 'MiniSnippetsSessionStart', callback = make_stop }
+
+vim.api.nvim_create_autocmd('User', opts)
 
 -- Allow snippets to show up in mini.complete
 MiniSnippets.start_lsp_server()
