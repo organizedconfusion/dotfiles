66dce82607c987a9f143f8309c8c94ad1c3ed13f
organizedconfusion <github@underclocked.com>
2026-04-16 09:16:17 -0700

    Turn off transparent lualine
    It kind of messed with readability in splits

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,18 +2,18 @@
 
 local auto_transparent = require('lualine.themes.auto')
 
-auto_transparent.normal.b.bg = 'None'
-auto_transparent.normal.c.bg = 'None'
-auto_transparent.insert.b.bg = 'None'
-auto_transparent.insert.c.bg = 'None'
-auto_transparent.visual.b.bg = 'None'
-auto_transparent.visual.c.bg = 'None'
-auto_transparent.replace.b.bg = 'None'
-auto_transparent.replace.c.bg = 'None'
-auto_transparent.command.b.bg = 'None'
-auto_transparent.command.c.bg = 'None'
-auto_transparent.inactive.b.bg = 'None'
-auto_transparent.inactive.c.bg = 'None'
+-- auto_transparent.normal.b.bg = 'None'
+-- auto_transparent.normal.c.bg = 'None'
+-- auto_transparent.insert.b.bg = 'None'
+-- auto_transparent.insert.c.bg = 'None'
+-- auto_transparent.visual.b.bg = 'None'
+-- auto_transparent.visual.c.bg = 'None'
+-- auto_transparent.replace.b.bg = 'None'
+-- auto_transparent.replace.c.bg = 'None'
+-- auto_transparent.command.b.bg = 'None'
+-- auto_transparent.command.c.bg = 'None'
+-- auto_transparent.inactive.b.bg = 'None'
+-- auto_transparent.inactive.c.bg = 'None'
 
 require('lualine').setup({
     options = { theme = auto_transparent }
